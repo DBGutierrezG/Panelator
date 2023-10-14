17a7f025bd7922686a6f95be6cf5930e30ddafc3
DBGutierrezG <145042524+DBGutierrezG@users.noreply.github.com>
2023-10-14 14:51:55 -0500

    Add files via upload

---
+++ Tarea1/src/tarea1/Tarea1.java
@@ -0,0 +1,15 @@
+package tarea1;
+
+/**
+ Daniel Gutierrez
+ */
+public class Tarea1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello\nDaniel Gutierrez!");
+    }
+    
+}
